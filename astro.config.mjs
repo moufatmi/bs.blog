fcf6a09b7e1060909561e0de14bd0206962ce078
moufatmi <mou.fatmi@edu.umi.ac.ma>
2025-06-22 02:04:12 +0100

    'update'

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
-  // @ts-check
-  import { defineConfig } from 'astro/config';
-  import tailwind from "@astrojs/tailwind";
+// @ts-check
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
 
-  export default defineConfig({
-    site: 'https://blog.beausejour.com',
-    output: 'static',
-    integrations: [tailwind()],
-    build: {
-      assets: '_astro',
-    },
-    trailingSlash: 'never',
-  });
+export default defineConfig({
+  site: 'https://blog.beausejourvoyage.com',
+  output: 'static',
+  integrations: [tailwind()],
+  build: {
+    assets: '_astro',
+  },
+  trailingSlash: 'never',
+});
