ecef377fe2ef1b4404ec8a13f4c538611b51863a
moufatmi <mou.fatmi@edu.umi.ac.ma>
2025-06-22 01:22:34 +0100

    good changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+  // @ts-check
+  import { defineConfig } from 'astro/config';
+  import tailwind from "@astrojs/tailwind";
 
-export default defineConfig({
-  site: 'https://moufatmi.github.io/Bs.blog', // Make sure 'blog-bs' here matches your repo name
-  base: '/Bs.blog/', // <--- THIS IS THE CRUCIAL PART TO CHANGE
-  integrations: [tailwind()],
-});
+  export default defineConfig({
+    site: 'https://moufatmi.github.io',
+    base: '/bs.blog/',
+    output: 'static',
+    integrations: [tailwind()],
+    build: {
+      assets: '_astro',
+    },
+    trailingSlash: 'never',
+  });
